8659621f779b32fe40033907073a7914614365bb
tahelka <73593127+tahelka@users.noreply.github.com>
2023-09-04 14:39:57 +0300

    Update NameInputUI.cs

--- RecycleTruck/Assets/Scripts/NameInputUI.cs
+++ RecycleTruck/Assets/Scripts/NameInputUI.cs
@@ -1,31 +1,14 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class NameInputUI : MonoBehaviour
 {
-    //private GameManager _gameManager;
-
-    private Button saveName;
-    //public TMP_InputField InputField { get; set; }
     [SerializeField] private TMP_InputField m_InputField;
     public TMP_InputField InputField
     {
         get { return m_InputField; }
         private set { m_InputField = value; }
     }
-
-
-
-
-    //private void Start()
-    //{
-    //}
-
-    //private void getMembersComponents()
-    //{
-    //    //InputField = transform.Find("NameInputField").GetComponent<TMP_InputField>();
-    //}
 
     public void SavePlayerName()
     {
